97a35ff0730e5a30eb041dc9735e00ef03861e91
seok <seokchan7952@gmail.com>
2024-03-18 19:23:59 +0900

    Initial commit

--- src/main/java/com/example/blogtest/service/UserService.java
+++ src/main/java/com/example/blogtest/service/UserService.java
@@ -3,9 +3,14 @@ package com.example.blogtest.service;
 import com.example.blogtest.domain.User;
 import com.example.blogtest.dto.AddUserRequest;
 import com.example.blogtest.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +31,11 @@ public class UserService {
                         .build()*/
         userRepository.save(new User(
                 request.getEmail(),encoder.encode(request.getPassword())));
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
 
